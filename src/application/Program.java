b3f6ca4123b705ca5a8aa390fc51ba3af485de8e
Alysson <alyssonn.bra@live.com>
2022-05-16 13:48:13 -0300

    First class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,14 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
     public static void main(String[] args) {
         
-        
-        System.out.print("hello world");
-
+        Position pos = new  Position(3, 5);
+        System.out.print(pos);
+       
     }
 
 }
