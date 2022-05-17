1fc2b7a5e9b171f3a3963ec1f14263221fd0bb85
Alysson <alyssonn.bra@live.com>
2022-05-17 16:48:39 -0300

    ChessException and ChessPosition

---
+++ src/chess/ChessException.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package chess;
+
+/**
+ *
+ * @author b246131
+ */
+public class ChessException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
+    public ChessException(String message) {
+        super(message);
+    }
+    
+   
+    
+}
