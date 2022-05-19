07ebf239bd47f49432ae6180b8cc96271cb1e884
Alysson <alyssonn.bra@live.com>
2022-05-19 17:03:39 -0300

    Implementing possible moves of Rook

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -53,7 +53,7 @@ public class ChessMatch {
             throw new ChessException("There is no piece on source position");
         }
         if(!board.piece(position).isThereAnyPossibleMove(position)){
-        throw new ChessException("There is no possible moves for the chosen piece")
+        throw new ChessException("There is no possible moves for the chosen piece");
         }
     }
 
