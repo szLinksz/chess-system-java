09630605bfa5bd4b7bab5e0e0ebb5a8ed2ffae11
Alysson <alyssonn.bra@live.com>
2022-05-23 13:32:33 -0300

    Check logic

--- src/chess/ChessPosition.java
+++ src/chess/ChessPosition.java
@@ -1,49 +1,38 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package chess;
 
 import boardgame.Position;
 
-/**
- *
- * @author b246131
- */
 public class ChessPosition {
-    
-    private char column;
-    private int row;
+	
+	private char column;
+	private int row;
+	
+	public ChessPosition(char column, int row) {
+		if (column < 'a' || column > 'h' || row < 1 || row > 8) {
+			throw new ChessException("Error instantiating ChessPosition. Valid values are from a1 to h8.");
+		}
+		this.column = column;
+		this.row = row;
+	}
 
-    public ChessPosition(char column, int row) {
-        if(column < 'a' || column > 'h' || row < 1 || row > 8){
-            throw new ChessException("Error instantiating ChessPosition. Valid values  are from a1 to h8");
-        }
-        this.column = column;
-        this.row = row;
-    }
+	public char getColumn() {
+		return column;
+	}
 
-    public char getColumn() {
-        return column;
-    }
+	public int getRow() {
+		return row;
+	}
 
-    public int getRow() {
-        return row;
-    }
-    
-    protected Position toPosition(){
-    
-    return new Position(8-row, column - 'a') ;
-    
-    }
-    
-    protected static ChessPosition fromPosition(Position position){
-        
-    return new ChessPosition((char)('a' - position.getColumn()), 8 - position.getRow());
-    
-    }
-    
-    public String toString(){
-    return "" + column+row; 
-    }
+	protected Position toPosition() {
+		return new Position(8 - row, column - 'a');
+	}
+	
+	protected static ChessPosition fromPosition(Position position) {
+		return new ChessPosition((char)('a' + position.getColumn()), 8 - position.getRow());
+	}
+	
+	@Override
+	public String toString() {
+		return "" + column + row;
+	}
 }
