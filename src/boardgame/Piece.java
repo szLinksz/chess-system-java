eb6d02c447c4171a61798ade9907351e4e6da28d
Alysson <alyssonn.bra@live.com>
2022-05-23 10:22:06 -0300

    Priting possible moves

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -2,37 +2,33 @@ package boardgame;
 
 public abstract class Piece {
 
-    protected Position position;
-    private Board board;
+	protected Position position;
+	private Board board;
+	
+	public Piece(Board board) {
+		this.board = board;
+		position = null;
+	}
 
-    public Piece(Board board) {
-        this.board = board;
-    }
-
-    protected Board getBoard() {
-        return board;
-    }
-
-    public abstract boolean[][] possibleMoves();
-
-    public boolean possibleMove(Position position) {
-
-        return possibleMoves()[position.getRow()][position.getColumn()];
-    }
-
-    public boolean isThereAnyPossibleMove(Position position) {
-
-        boolean mat[][] = possibleMoves();
-        for (int i = 0; i < mat.length; i++) {
-            for (int j = 0; j < mat[i].length; j++) {
-                if (mat[i][j]) {
-                    return true;
-                }
-
-            }
-
-        }
-        return false;
-    }
-
+	protected Board getBoard() {
+		return board;
+	}
+	
+	public abstract boolean[][] possibleMoves();
+	
+	public boolean possibleMove(Position position) {
+		return possibleMoves()[position.getRow()][position.getColumn()];
+	}
+	
+	public boolean isThereAnyPossibleMove() {
+		boolean[][] mat = possibleMoves();
+		for (int i=0; i<mat.length; i++) {
+			for (int j=0; j<mat.length; j++) {
+				if (mat[i][j]) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
 }
--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -1,29 +1,24 @@
-
 package chess;
 
 import boardgame.Board;
 import boardgame.Piece;
 import boardgame.Position;
 
+public abstract class ChessPiece extends Piece {
 
-public abstract class ChessPiece extends Piece{
+	private Color color;
 
-private Color color; 
+	public ChessPiece(Board board, Color color) {
+		super(board);
+		this.color = color;
+	}
 
-    public ChessPiece(Board board, Color color) {
-        super(board);
-        this.color = color;
-    }
-
-    public Color getColor() {
-        return color;
-    }
-    
-    protected boolean isThereOpponentPiece(Position position){
-        ChessPiece p = (ChessPiece)getBoard().piece(position);
-        return p !=  null && p.getColor() != color;   
-    }
- 
-    
-    
+	public Color getColor() {
+		return color;
+	}
+	
+	protected boolean isThereOpponentPiece(Position position) {
+		ChessPiece p = (ChessPiece)getBoard().piece(position);
+		return p != null && p.getColor() != color;
+	}
 }
